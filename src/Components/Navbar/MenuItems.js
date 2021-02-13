7ac07254e153ea8f31367d99854a48d6b798c9cb
Roohanjyot <Roohansidhu@gmail.com>
2021-02-13 00:11:30 -0800

    Initial set up

---
+++ src/Components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: "Home",
+        url: '#',
+        cName: 'nav-link'
+    },
+    {
+        title: "Table View",
+        url: '#',
+        cName: 'nav-link'
+    },
+    {
+        title: "Chart View",
+        url: '#',
+        cName: 'nav-link'
+    },
+    {
+        title: "Track",
+        url: '#',
+        cName: 'nav-link'
+    },
+    {
+        title: "Burger Icon",
+        url: '#',
+        cName: 'nav-link-mobile'
+    }
+]
